0af9c06bf27b9b9dedfbc872f5bd57f7eaa2d591
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-01-12 09:30:14 +0100

    App HTML and CSS done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 
-const App = () =>{
+const App: FC = () => {
   return (
     <div className="App">
-      <h1>Ούλε τε και μάλα χαίρε</h1>
+      <h1>Κατάλογος Εργασιών</h1>
+      <h6>(React & Typescript)</h6>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type='text' placeholder='Task...'/>
+          <input type='text' placeholder='Deadline (in Days)...'/>
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todoList'></div>
     </div>
   );
 }
